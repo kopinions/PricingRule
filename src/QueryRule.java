ec483dcb5753bdd529e67e70836d84cf06663e06
jksun <jksun@thoughtworks.com>
2014-06-22 16:12:30 +0800

    clean the code

--- src/QueryRule.java
+++ src/QueryRule.java
@@ -3,13 +3,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-/**
- * Created with IntelliJ IDEA.
- * User: twer
- * Date: 6/17/14
- * Time: 4:12 PM
- * To change this template use File | Settings | File Templates.
- */
 public class QueryRule {
     private HashMap<String, String> rule;
 
@@ -18,15 +11,15 @@ public class QueryRule {
     }
 
     public int match(HashMap<String, String> queryCriteir) {
-        List<String> keyset = rule.entrySet().stream().map(e -> e.getKey()).collect(Collectors.toList());
-        Set<Boolean> collect1 = keyset.stream().map(key ->
-               rule.getOrDefault(key, "VALUE").equals(queryCriteir.getOrDefault(key, "QUERY"))).collect(Collectors.toSet());
-        if (collect1.size() == 1) {
-            if (collect1.contains(false)) {
+        List<String> ruleHeaders = rule.entrySet().stream().map(e -> e.getKey()).collect(Collectors.toList());
+        Set<Boolean> matchStatus = ruleHeaders.stream().map(key ->
+                rule.getOrDefault(key, "VALUE").equals(queryCriteir.getOrDefault(key, "QUERY"))).collect(Collectors.toSet());
+        if (matchStatus.size() == 1) {
+            if (matchStatus.contains(false)) {
                 return -1;
             }
             return 1;
-        } else{
+        } else {
             return 0;
         }
     }
--- src/QueryRuleTable.java
+++ src/QueryRuleTable.java
@@ -3,33 +3,24 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-/**
- * Created with IntelliJ IDEA.
- * User: twer
- * Date: 6/17/14
- * Time: 4:42 PM
- * To change this template use File | Settings | File Templates.
- */
 public class QueryRuleTable {
     private Map<QueryRule, Integer> queryRule;
-    private int defaultValue =0;
+    private int defaultValue =1;
 
     public QueryRuleTable(Map<QueryRule, Integer> queryRule) {
-
         this.queryRule = queryRule;
     }
 
-
-
     public int getPrice(HashMap<String, String> rule) {
-        List<Map.Entry<QueryRule, Integer>> collect = queryRule.entrySet().stream().filter(r -> r.getKey().match(rule) == 0 || r.getKey().match(rule) == 1).collect(Collectors.toList());
-        if (collect.size() == 1) {
-            return collect.get(0).getValue();
+        List<Map.Entry<QueryRule, Integer>> matchQueryRules = queryRule.entrySet().stream().filter(r -> r.getKey().match(rule) == 0 || r.getKey().match(rule) == 1).collect(Collectors.toList());
+        if (matchQueryRules.size() == 1) {
+            return matchQueryRules.get(0).getValue();
         }
-        if (collect.size() == 0) {
-            return 0;
+        if (matchQueryRules.size() == 0) {
+            return defaultValue;
         }
-        return collect.stream().max((c1, c2) -> c1.getValue() - c2.getValue()).get().getValue();
+        Integer maxPrice = matchQueryRules.stream().max((c1, c2) -> c1.getValue() - c2.getValue()).get().getValue();
+        return maxPrice;
     }
 
     public int getDefaultValue() {
